21070deb312b84a1a2ffe4aab4e93bdd929de23a
Ahmad Raza <raza82504@gmail.com>
2021-04-08 08:06:55 +0530

    created my portfolio website

---
+++ mystory.js
@@ -0,0 +1,15 @@
+
+  document.addEventListener('DOMContentLoaded', function(options) {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+   document.addEventListener('DOMContentLoaded', function(options) {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function(options) {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
---
+++ projects.js
@@ -0,0 +1,9 @@
+
+  document.addEventListener('DOMContentLoaded', function(options) {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+   document.addEventListener('DOMContentLoaded', function(options) {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
